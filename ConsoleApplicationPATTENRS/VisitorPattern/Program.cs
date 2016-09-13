afd79520e2249126cff8412c644d3904567a44fc
Heitor Helmer Herzog <Heitor Helmer Herzog>
2016-09-12 23:45:27 -0300

    Adicionado : classe Extensions formatar double 2 casas IVisitor Liquor Tabacco Necesity TaxVisitor
    Exibindo dados no console

--- ConsoleApplicationPATTENRS/VisitorPattern/Program.cs
+++ ConsoleApplicationPATTENRS/VisitorPattern/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VisitorPattern
 {
@@ -10,7 +6,26 @@ namespace VisitorPattern
     {
         static void Main( string[] args )
         {
+            TaxVisitor taxCal = new TaxVisitor();
 
+            Necesity milk = new Necesity(3.47f);
+            Liquor vodka = new Liquor(10f);
+            Tabacco cigars = new Tabacco(12f);
+
+            Console.WriteLine(milk.Accept(taxCal));
+            Console.WriteLine(vodka.Accept(taxCal));
+            Console.WriteLine(cigars.Accept(taxCal));
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static double Format(this double item)
+        {
+            return Convert.ToDouble(
+                String.Format("{0:00.0}",item));
         }
     }
 
@@ -20,27 +35,89 @@ namespace VisitorPattern
          double Visit( Liquor liquorItem);
          double Visit( Tabacco tabaccoItem );
          double Visit( Necesity necessityItem );
+    }
 
+    public interface IVisible
+    {
+        double Accept( IVisitor visitor );
+    }
+    public class Liquor:IVisible 
+    {
+        private double price;
+       public Liquor(double item)
+        {
+            price = item;
+        }
+        
+        public double GetPrice()
+        {
+            return price;
+        }
+
+        public double Accept( IVisitor visitor )
+        {
+            return visitor.Visit(this);
+        }
+    }
+
+    public class Tabacco:IVisible
+    {
+        private double price;
+        public Tabacco( double item )
+        {
+            price = item;
+        }
+        public double GetPrice( )
+        {
+            return price;
+        }
+
+        public double Accept( IVisitor visitor )
+        {
+            return visitor.Visit(this);
+        }
+    }
+
+    public class Necesity:IVisible
+    {
+        private double price;
+        
+        public Necesity( double item )
+        {
+            price = item;
+        }
+        public double GetPrice( )
+        {
+            return price;
+        }
+
+        public double Accept( IVisitor visitor )
+        {
+            return visitor.Visit(this);
+        }
     }
 
     public class TaxVisitor:IVisitor
     {
-        public double Visit( Necesity NecessityItem )
+        public double Visit( Necesity necessityItem )
         {
             Console.WriteLine("Necesity item : Price with Tax");
-            return necessityItem.GetPrice() * .18f + necessityItem.GetPrice();
+            return (necessityItem.GetPrice() * 0f
+                + necessityItem.GetPrice()).Format();
         }
 
         public double Visit( Tabacco tabaccoItem )
         {
             Console.WriteLine("Tabacco item : Price with Tax");
-            return tabaccoItem.GetPrice() * .18f + tabaccoItem.GetPrice();
+            return (tabaccoItem.GetPrice() * .32f 
+                + tabaccoItem.GetPrice()).Format();
         }
 
         public double Visit( Liquor liquorItem )
         {
             Console.WriteLine("Liquor item : Price with Tax");
-            return liquorItem.GetPrice() * .18f + liquorItem.GetPrice();
+            return (liquorItem.GetPrice() * .18f 
+                + liquorItem.GetPrice()).Format();
         }
     }
 }
